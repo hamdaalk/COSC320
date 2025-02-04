584d638f6a530cfc11b3ad3ecdc4f0ec3023e5b2
hamdaalk <125359115+hamdaalk@users.noreply.github.com>
2025-02-04 10:57:31 +0400

    Add files via upload

---
+++ 04_primitive_types/primitive_types4.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // You can optionally experiment here.
+        let a = [1, 2, 3, 4, 5];
+        let nice_slice = &a[1..4];
+        assert_eq!([2, 3, 4], nice_slice);
+        println!("Slice: {:?}", nice_slice);
+        if nice_slice == [2, 3, 4] {
+            println!("Test passed!");
+        } else {
+            println!("Test failed!");
+        }
+
+        
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn slice_out_of_array() {
+        let a = [1, 2, 3, 4, 5];
+
+        // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
+        let nice_slice = &a[1..4];
+
+        assert_eq!([2, 3, 4], nice_slice);
+    }
+}
